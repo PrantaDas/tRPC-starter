3b6ad5a1c32a6286a8961c7ef6cff83d107658d4
Prantadas <prantodas043@gmail.com>
2025-02-25 00:02:21 +0600

    update: the error class updated

--- src/errors/appError.ts
+++ src/errors/appError.ts
@@ -5,6 +5,11 @@ export class AppError extends Error {
     super(message);
     this.statusCode = statusCode;
     this.name = "AppError";
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, this.constructor);
+    } else {
+      this.stack = new Error().stack;
+    }
     Object.setPrototypeOf(this, new.target.prototype);
   }
 }
